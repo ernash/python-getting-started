09239f040a6c166a9f2b8ba310ee461e44d642d2
ernash <ernash@brainhash.com>
2015-01-25 19:07:37 +0100

    Initialized project with Hello World example

---
+++ hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+    Prints "Hello World".
+'''
+print('Hello World')
+
+'''
+    Prints "世界今日は", the japanese equivalent for "Hello World".
+    This will only work in Python 2.7 if utf-8 encoding is enabled for this 
+    file, in Python 3 this is enabled by default.
+    
+    Add the following line in the beginning of the python file:
+        # -*- coding: utf-8 -*-
+    to enable UTF-8 encoding for Python 2.7.
+'''
+print('世界今日は')
