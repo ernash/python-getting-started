d137a331a6b591606a3ed5546dcbd2324a47b371
ernash <ernash@brainhash.com>
2015-01-25 19:48:43 +0100

    count in a loop from 1 to 10

---
+++ count.py
@@ -0,0 +1,8 @@
+print('Count from 1 to 10')
+'''
+    Counts from 1 to 10 and prints the number.
+    The 11 in "range(1, 11)" is exclusive, so it will only print the numbers up
+    to 10.
+'''
+for count in range(1, 11):
+    print(count)
